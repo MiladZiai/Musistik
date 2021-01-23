a2f9cd20a2f8802655a2cb684aa88e9defe514ff
Milad Ziai <milad.ziai@gmail.com>
2021-01-23 17:54:55 +0100

    navbar done

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ const path = require('path')
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.set('views', path.join("", "views"));
 app.engine('hbs', expressHandlebars({
   defaultLayout: 'main.hbs',
 }))
 
+app.use(express.static(path.join(__dirname, '/views/public')))
+
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.render("home.hbs")
+})
+
+app.get('/about', (req, res) => {
+  res.render("about.hbs")
+})
+
+app.get('/contact', (req, res) => {
+  res.render("contact.hbs")
 })
 
 app.listen(8080, () => {
