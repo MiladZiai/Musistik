b1d195eb2bd550cfe2945e6cb19ad2f3a35be37b
Milad Ziai <milad.ziai@gmail.com>
2021-04-19 15:48:22 +0200

    added the songs from triple store to sqliteDB

--- graphDB.js
+++ graphDB.js
@@ -1,11 +1,13 @@
 const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client');
+const db = require('./db')
+
 
 // connection data to the running GraphDB instance
 const GRAPHDB_BASE_URL = 'http://localhost:7200',
     GRAPHDB_REPOSITORY = 'myrepo',
     GRAPHDB_USERNAME = 'admin',
     GRAPHDB_PASSWORD = 'test',
-    GRAPHDB_CONTEXT_TEST = 'http://ont.enapso.com/repo';
+    GRAPHDB = 'http://www.openrdf.org/schema/sesame#nil';
 
 const DEFAULT_PREFIXES = [
     EnapsoGraphDBClient.PREFIX_OWL,
@@ -16,6 +18,18 @@ const DEFAULT_PREFIXES = [
     {
         prefix: 'entest',
         iri: 'http://ont.enapso.com/test#'
+    },
+    {
+        prefix: 'dbo',
+        iri: 'http://dbpedia.org/ontology/'
+    },
+    {
+        prefix: 'foaf',
+        iri: 'http://xmlns.com/foaf/spec/'
+    },
+    {
+        prefix: 'mo',
+        iri: 'http://purl.org/ontology/mo/'
     }
 ];
 
@@ -27,9 +41,34 @@ let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
 });
 
 graphDBEndpoint.login(GRAPHDB_USERNAME, GRAPHDB_PASSWORD)
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((err) => {
-        console.log(err);
+.then((result) => {
+    console.log(result);
+})
+.catch((err) => {
+    console.log(err);
+});
+
+//selects the songs from graphDB and for each record adds it to sqliteDB
+graphDBEndpoint.query(
+    `select * from <${GRAPHDB}>
+        where {
+            ?x foaf:name ?name .
+            ?x mo:MusicArtist ?artist .
+            ?x mo:genre ?genre .
+        }`
+)
+.then((result) => {
+    result.records.forEach(element => {
+        const model = {
+            title: element.name,
+            artist: element.artist,
+            genre: element.genre
+        }
+        db.addSong(model, function(error){
+            console.log(error);
+        })
     });
+})
+.catch((err) => {
+    console.log(err);
+});
